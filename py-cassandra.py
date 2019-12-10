657b80ccada00b9a5e158d530b24f66af6986b87
yuanyaru <398916756@qq.com>
2019-12-10 09:23:35 +0800

    update

---
+++ py-cassandra.py
@@ -0,0 +1,7 @@
+# /usr/bin/python
+
+from cassandra.cluster import Cluster
+cluster = Cluster(["192.168.100.62"])
+session = cluster.connect()
+session.execute("CREATE KEYSPACE test WITH replication = {'class': 'SimpleStrategy', 'replication_factor': '3'};")
+print cluster.metadata.keyspaces
